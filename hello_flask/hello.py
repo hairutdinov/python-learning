7735980c2def39d78de51f34545944dda1e96f6f
bulat <bulatxajrutdinoff@yandex.ru>
2023-08-05 10:48:53 +0300

    flask: variables in URL

--- hello_flask/hello.py
+++ hello_flask/hello.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/hello/<name>")
+def greeting(name):
+    return f'Hello, {name}'
+
 if __name__ == '__main__':
     app.run()
