adc4762153bd0c88a8687e40c656febc863a92b9
bulat <bulatxajrutdinoff@yandex.ru>
2023-08-07 14:22:33 +0300

    make bold, emphasis, underlined decorators

--- hello_flask/hello.py
+++ hello_flask/hello.py
@@ -2,8 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def make_bold(func):
+    return lambda: '<b>{}</b>'.format(func())
+def make_emphasis(func):
+    return lambda: '<em>{}</em>'.format(func())
+def make_underlined(func):
+    return lambda: '<u>{}</u>'.format(func())
+
 
 @app.route("/")
+@make_bold
+@make_underlined
+@make_emphasis
 def hello_world():
     return "<p>Hello, World!</p>"
 
@@ -12,4 +22,4 @@ def greeting(name):
     return f'Hello, {name}'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
